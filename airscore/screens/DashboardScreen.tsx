84769e3c42c27deb46cfefd6956f5e7cc4a19e04
Joshua Layne <joshualayne2684@gmail.com>
2025-07-20 22:26:11 -0400

    Added custom header for dashboard

--- airscore/screens/DashboardScreen.tsx
+++ airscore/screens/DashboardScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useRef, useLayoutEffect } from 'react';
 import { RefreshControl, View, ScrollView, Text, FlatList, SectionList, Animated, Button, TouchableOpacity, ActivityIndicator, StyleSheet } from 'react-native';
 
 import { useNavigation } from "@react-navigation/native";
@@ -13,10 +13,39 @@ import MusicItemCard from '../components/MusicItemCard';
 const DashboardScreen = ({}) => {
     const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>();
 
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            header: () => (
+                <View
+                    style={{
+                        height: 100,
+                        backgroundColor: 'white',
+                        justifyContent: 'flex-end',
+                        padding: 12,
+                    }}
+                >
+                <Text
+                    style={{
+                        color: 'black',
+                        fontWeight: '300',
+                        fontSize: 24,
+                        padding: 12,
+                        marginLeft: 20,
+                        marginTop: 12,
+                    }}
+                >
+                    AirScore
+                </Text>
+                </View>
+            ),
+            });
+
+    }, [navigation]);
+
     return (
         <View className="flex-1 bg-white">
             {/* Quick Options Menu */}
-            <View className="flex-row justify-between p-4 mt-8 mb-4 bg-white shadow-md w-[75%] self-center rounded-lg">
+            <View className="flex-row justify-between p-4 mt-4 mb-4 bg-white shadow-md w-[75%] self-center rounded-lg">
                 {/* Library */}
                 <View className="flex-1 justify-center items-center">
                     <TouchableOpacity className="p-2 m-1 rounded justify-center items-center w-full" onPress={() => navigation.navigate('Library')}>
