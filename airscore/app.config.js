2df213c2665017708288c46ab3157cd5b1a0696f
Joshua Layne <joshua.r.layne@gmail.com>
2026-06-30 15:21:49 -0400

    Updated versioning

--- airscore/app.config.js
+++ airscore/app.config.js
@@ -15,7 +15,7 @@ export default ({ config }) => ({
   name: "AirScore",
   slug: "airscore",
   scheme: "airscore",
-  version: "1.0.0",
+  version: "1.0.1",
   orientation: "default",
   icon: "./assets/adaptive-icon.png",
   userInterfaceStyle: "light",
@@ -33,7 +33,7 @@ export default ({ config }) => ({
       foregroundImage: "./assets/adaptive-icon.png",
       backgroundColor: "#0099FF",
     },
-    versionCode: 3,
+    versionCode: 4,
     edgeToEdgeEnabled: true,
     package: "com.jlayne246.airscore"
   },
